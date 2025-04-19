1fff3707e5b0da6b781ae7aa8ea3d6977a41f3d2
刘恒甲 <stillsb@qq.com>
2025-04-19 11:07:38 +0800

    upd

--- gitpush.bat
+++ gitpush.bat
@@ -1,21 +1,30 @@
 @echo off
 chcp 65001 > nul
-echo 开始提交并上传当前文件夹的Git仓库...
+
+rem 检查是否有未提交的更改
+git diff --quiet --exit-code
+if %errorlevel% equ 0 (
+    echo 本地没有修改，无需提交。
+    pause
+    exit /b
+)
+
+echo 开始提交并上传当前文件夹的 Git 仓库...
 git add .
 if %errorlevel% neq 0 (
-    echo 执行git add .时出错！
+    echo 执行 git add . 时出错！
     pause
     exit /b
 )
 git commit -m "upd"
 if %errorlevel% neq 0 (
-    echo 执行git commit -m "upd"时出错！
+    echo 执行 git commit -m "upd" 时出错！
     pause
     exit /b
 )
 git push
 if %errorlevel% neq 0 (
-    echo 执行git push时出错！
+    echo 执行 git push 时出错！
     pause
     exit /b
 )
